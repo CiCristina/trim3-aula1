f42e08809576877fd347380b943dd38a619cc181
Cibelly Cristina <cibellycristinaudi@gmail.com>
2017-10-05 15:33:48 -0300

    Continuar programacao do Player

---
+++ Assets/Scripts/PlayerCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+
+public float horizontalSpeed = 10f;
+
+// Use this for initialization
+void Start () {
+
+}
+
+// Update is called once per frame
+void Update () {
+
+float horizontalInput = Input.GetAxisRaw("Horizontal"); 
+}
+
+
+void MoveHorizontal(float speed) {
+
+}
+
+
+
+
+void StopMoving() {
+
+}
