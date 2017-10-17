fba123d50f38145108e62c5439bf87fdf45dd42e
Amanda Abrao <amandaabrao55@gmail.com>
2017-10-17 09:46:36 -0200

    Movimentacao basica na horizontal

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -7,25 +7,34 @@ public class PlayerCtrl : MonoBehaviour {
 
 public float horizontalSpeed = 10f;
 
+Rigidbody2D rb;
+
 // Use this for initialization
 void Start () {
+    rb = GetComponent<Rigidbody2D>();
 
 }
 
 // Update is called once per frame
 void Update () {
 
-float horizontalInput = Input.GetAxisRaw("Horizontal"); 
+float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1: esquerda, 1: direita 
+float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
+if (horizontalPlayerSpeed != 0){
+    MoveHorizontal(horizontalPlayerSpeed);
 }
-
+else {
+    StopMovingHorizontal();
+    }
+}
 
 void MoveHorizontal(float speed) {
+    rb.velocity = new Vector2(speed, rb.velocity.y);
 
 }
 
+void StopMovingHorizontal() {
+    rb.velocity = new Vector2 (0f, rb.velocity.y);
 
-
-
-void StopMoving() {
-
+}
 }
